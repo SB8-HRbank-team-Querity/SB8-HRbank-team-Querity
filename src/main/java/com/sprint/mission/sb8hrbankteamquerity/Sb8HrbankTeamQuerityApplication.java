4d09cff3a2bc83ec3fb9023697440d3a7273ad94
dev-jin8612 <workspace8612@gmail.com>
2026-01-20 22:38:19 +0900

    push test

---
+++ src/main/java/com/sprint/mission/sb8hrbankteamquerity/Sb8HrbankTeamQuerityApplication.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.sb8hrbankteamquerity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb8HrbankTeamQuerityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb8HrbankTeamQuerityApplication.class, args);
+    }
+
+}
