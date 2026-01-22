6a0b612374dd629d79a97c171b308bd9087e6866
yeeun000 <dldpdms0000@naver.com>
2026-01-22 23:08:21 +0900

    feat : Imple Employee Detail Search

--- src/main/java/com/sprint/mission/sb8hrbankteamquerity/service/EmployeeService.java
+++ src/main/java/com/sprint/mission/sb8hrbankteamquerity/service/EmployeeService.java
@@ -1,8 +1,10 @@
 package com.sprint.mission.sb8hrbankteamquerity.service;
 
+import com.sprint.mission.sb8hrbankteamquerity.dto.employee.EmployeeDto;
 import com.sprint.mission.sb8hrbankteamquerity.dto.employee.EmployeePageResponse;
 import com.sprint.mission.sb8hrbankteamquerity.dto.employee.EmployeeSearchDto;
 
 public interface EmployeeService {
     EmployeePageResponse findAll(EmployeeSearchDto Dto);
+    EmployeeDto findById(Long id);
 }
