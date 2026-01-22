0164630845d6e43c03ddc9c9da49a6c08f70b68f
dev-jin8612 <workspace8612@gmail.com>
2026-01-22 11:23:34 +0900

    reactor: EmployeeHistoryType edit

--- src/main/java/com/sprint/mission/sb8hrbankteamquerity/entity/EmployeeHistoryType.java
+++ src/main/java/com/sprint/mission/sb8hrbankteamquerity/entity/EmployeeHistoryType.java
@@ -1,17 +1,7 @@
 package com.sprint.mission.sb8hrbankteamquerity.entity;
 
 public enum EmployeeHistoryType {
-    ADD("직원 추가"),
-    UPDATE("정보 수정"),
-    DEL("직원 삭제");
-
-    private final String label;
-
-    EmployeeHistoryType(String label) {
-        this.label = label;
-    }
-
-    public String getLabel() {
-        return label;
-    }
+    CREATED,
+    UPDATED,
+    DELETED
 }
