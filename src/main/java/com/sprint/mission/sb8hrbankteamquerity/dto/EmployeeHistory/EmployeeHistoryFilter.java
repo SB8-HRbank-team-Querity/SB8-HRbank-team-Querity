b9922f2ffba4e51fa2b85a8aa0a4752991d11554
dev-jin8612 <workspace8612@gmail.com>
2026-01-24 19:52:12 +0900

    for save

--- src/main/java/com/sprint/mission/sb8hrbankteamquerity/dto/EmployeeHistory/EmployeeHistoryFilter.java
+++ src/main/java/com/sprint/mission/sb8hrbankteamquerity/dto/EmployeeHistory/EmployeeHistoryFilter.java
@@ -6,11 +6,17 @@ import org.springframework.data.repository.query.Param;
 import java.time.Instant;
 
 public record EmployeeHistoryFilter(
-    @Param("number") String employeeNumber,
-    @Param("memo") String memo,
-    @Param("ip") String ip,
-    @Param("startDate") Instant startDate,
-    @Param("endDate") Instant endDate,
-    @Param("type") EmployeeHistoryType type
+//    @Param("number") String employeeNumber,
+//    @Param("memo") String memo,
+//    @Param("ip") String ip,
+//    @Param("startDate") Instant startDate,
+//    @Param("endDate") Instant endDate,
+//    @Param("type") EmployeeHistoryType type
+    String employeeNumber,
+     String memo,
+    String ip,
+    Instant startDate,
+    Instant endDate,
+     EmployeeHistoryType type
 ) {
 }
