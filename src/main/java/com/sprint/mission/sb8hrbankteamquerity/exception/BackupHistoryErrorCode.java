c6075647f9381d482a3b99c1ef106bf8acf88cd8
jaejo <jaejoon0520@naver.com>
2026-01-26 14:45:25 +0900

    "fect: style: BackupHistoryErrorCode numeric change only"

--- src/main/java/com/sprint/mission/sb8hrbankteamquerity/exception/BackupHistoryErrorCode.java
+++ src/main/java/com/sprint/mission/sb8hrbankteamquerity/exception/BackupHistoryErrorCode.java
@@ -7,8 +7,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum BackupHistoryErrorCode implements ErrorCode {
-    BACKUP_ALREADY_IN_PROGRESS(6001, "ALREADY_IN_PROGRESS", HttpStatus.CONFLICT, "이미 진행 중인 백업이 있습니다."),
-    BACKUP_PARAMETER_BAD_REQUEST(6002, "INVALID_SORT_FIELD", HttpStatus.BAD_REQUEST, "지원하지 않는 정렬 필드입니다.");
+    BACKUP_ALREADY_IN_PROGRESS(4001, "ALREADY_IN_PROGRESS", HttpStatus.CONFLICT, "이미 진행 중인 백업이 있습니다."),
+    BACKUP_PARAMETER_BAD_REQUEST(4002, "INVALID_SORT_FIELD", HttpStatus.BAD_REQUEST, "지원하지 않는 정렬 필드입니다.");
 
     private final int numeric;
     private final String errorKey;
