2a9d284c1b513b1a1394dbf1cbf7bd816fac18c7
parkhamin <parkhamint@naver.com>
2026-01-26 18:32:39 +0900

    feat: Implement Excel data reading with Apache POI

--- src/main/java/com/sprint/mission/sb8hrbankteamquerity/exception/DepartmentErrorCode.java
+++ src/main/java/com/sprint/mission/sb8hrbankteamquerity/exception/DepartmentErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum DepartmentErrorCode implements ErrorCode {
     DEPT_NOT_FOUND(1001, "NOT_FOUND", HttpStatus.NOT_FOUND, "해당 부서를 찾을 수 없습니다."),
     DUPLICATE_DEPT_NAME(1002, "DUPLICATE_NAME", HttpStatus.CONFLICT, "이미 존재하는 부서 이름입니다."),
-    DEPT_NOT_EMPTY(1003, "NOT_EMPTY", HttpStatus.CONFLICT, "해당 부서에 소속된 직원이 있어 삭제할 수 없습니다.");
+    DEPT_NOT_EMPTY(1003, "NOT_EMPTY", HttpStatus.CONFLICT, "해당 부서에 소속된 직원이 있어 삭제할 수 없습니다."),
+    INVALID_EXCEL_FILE(1004, "INVALID_FILE", HttpStatus.BAD_REQUEST, "엑셀 파일 처리 중 오류가 발생했습니다.");
 
     private final int numeric;
     private final String errorKey;
