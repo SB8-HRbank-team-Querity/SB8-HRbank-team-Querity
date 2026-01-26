b84c252c1ba2d2cc84d736cf317547e129f67392
jaejo <jaejoon0520@naver.com>
2026-01-26 17:14:58 +0900

    "feat: apply swagger document"

--- src/main/java/com/sprint/mission/sb8hrbankteamquerity/service/BackupHistoryService.java
+++ src/main/java/com/sprint/mission/sb8hrbankteamquerity/service/BackupHistoryService.java
@@ -1,15 +1,15 @@
 package com.sprint.mission.sb8hrbankteamquerity.service;
 
-import com.sprint.mission.sb8hrbankteamquerity.dto.BackupHistory.BackupHistoryPageRequest;
 import com.sprint.mission.sb8hrbankteamquerity.dto.BackupHistory.BackupHistoryDto;
+import com.sprint.mission.sb8hrbankteamquerity.dto.BackupHistory.BackupHistoryPageRequest;
 import com.sprint.mission.sb8hrbankteamquerity.dto.BackupHistory.CursorPageResponseBackupHistoryDto;
 import com.sprint.mission.sb8hrbankteamquerity.entity.BackupHistoryStatus;
 
 public interface BackupHistoryService {
 
-    BackupHistoryDto create(String worker);
-
     CursorPageResponseBackupHistoryDto findAll(BackupHistoryPageRequest request);
+
+    BackupHistoryDto create(String worker);
 
     BackupHistoryDto findLatestByStatus(BackupHistoryStatus status);
 }
